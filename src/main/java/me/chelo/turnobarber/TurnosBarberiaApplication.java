9fa8d312895d7e842ec0f6e7ce9bc35befbb3d67
TheSkillex12 <chelotoara@gmail.com>
2024-06-04 15:16:12 -0300

    Subiendo proyecto base

---
+++ src/main/java/me/chelo/turnobarber/TurnosBarberiaApplication.java
@@ -0,0 +1,13 @@
+package me.chelo.turnobarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosBarberiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosBarberiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/chelo/turnobarber/TurnosBarberiaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chelo.turnobarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosBarberiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
